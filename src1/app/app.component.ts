9c0d4a788c7c939f342af3c142e26026700c42ab
felipezzz1 <80336800+felipezzz1@users.noreply.github.com>
2022-09-11 20:34:46 -0300

    Add files via upload

---
+++ src1/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Songs', url: '/home', icon: 'heart' },
+    { title: 'Detail', url: '/details', icon: 'people' },
+    { title: 'Add Songs', url: '/cadastrar', icon: 'add' },
+  ];
+  constructor() {}
+}
