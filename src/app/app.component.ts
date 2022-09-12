3eeff7aedc94a97f609ed4a8cbcc498cb6c1f1b9
Vinicius Alves Beló <76420195+ViniBelo@users.noreply.github.com>
2022-09-11 23:22:21 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Artists', url: '/folder/Artists', icon: 'people' },
-    { title: 'Albums', url: '/folder/Albums', icon: 'albums' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Playlists', url: '/folder/Playlists', icon: 'musical-note' },
-    { title: 'Genre', url: '/folder/Genre', icon: 'musical-notes' },
-    { title: 'Add Songs', url: 'folder/Add', icon: 'add' },
+    { title: 'Songs', url: '/home', icon: 'heart' },
+    { title: 'Detail', url: '/details', icon: 'people' },
+    { title: 'Add Songs', url: '/cadastrar', icon: 'add' },
   ];
   constructor() {}
 }
